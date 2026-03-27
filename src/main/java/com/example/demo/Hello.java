8efffabcee0400b1f192dca340a3f02277a2e94c
Javier Choque <jdka.suburbio@gmail.com>
2026-03-27 09:12:31 -0400

    Add Method POST

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class Hello {
 
+    ArrayList<String> list = new ArrayList<>();
+
     @GetMapping("/hello")
     public String GetHello()
     {
-        return "Hello World";
+        String text = "";
+        for (int i = 0; i < list.size(); i++) {
+            text = text + list.get(i);
+        }
+        return text;
+    }
+
+    @PostMapping("/hello")
+    public String PostHello(@RequestBody String variable)
+    {
+        list.add(variable);
+        return  variable;
     }
 }
